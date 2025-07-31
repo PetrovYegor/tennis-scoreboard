66496dd619a0ea40011c638d857555101aff45af
PetrovYegor <YegorPetrov@outlook.com>
2025-08-01 00:06:07 +0300

    Добавил в pom.xml зависимость org.glassfish.expressly для поддержки интерполяции строк в аннотациях, добавил основные JPA и JVA аннотации для сущности Player

--- src/main/java/com/github/petrovyegor/tennisscoreboard/model/Player.java
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/model/Player.java
@@ -1,18 +1,34 @@
 package com.github.petrovyegor.tennisscoreboard.model;
 
-import lombok.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Entity
+@Table(name = "Players")
 @Getter
 @ToString
 @NoArgsConstructor
-@RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    @Positive(message = "ID should be positive number")
     @EqualsAndHashCode.Include
     private int id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    @Size(min = 3, max = 30, message = "Name should be from {min} to {max} symbols")
+    @NotBlank(message = "Name can not be null or contain only white spaces")
     private String name;
 
-    public Player(String name){
+    public Player(String name) {
         this.name = name;
     }
 }

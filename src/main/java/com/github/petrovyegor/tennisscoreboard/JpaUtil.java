01a9ecb716bbda50ab7c051130faac7d3abe7f15
PetrovYegor <YegorPetrov@outlook.com>
2025-08-01 23:47:43 +0300

    Создал xml файл с указанием драйвера, урл, логина и пароля для подключения к БД. Сделал класс PlayerDao, создал класс JpaUtil, который будет дать соедение с БД

---
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/JpaUtil.java
@@ -0,0 +1,27 @@
+package com.github.petrovyegor.tennisscoreboard;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtil {
+    private static final EntityManagerFactory entityManagerFactory;
+
+    static {
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("h2-unit");
+        } catch (Throwable e) {
+            throw new RuntimeException("Initialization of EntityManagerFactory was failed." + e);
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    public static void close() {
+        if (entityManagerFactory != null) {
+            entityManagerFactory.close();
+        }
+    }
+}

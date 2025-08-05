64e877bd98e76b768ac27d965341a1fee3d7041f
PetrovYegor <YegorPetrov@outlook.com>
2025-08-05 22:34:40 +0300

    Доработал модель Player, чтобы в name можно было вводить только латинских буквы. Уменьшил максимальную длину имени до 15-ти символов

--- src/main/java/com/github/petrovyegor/tennisscoreboard/model/Player.java
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/model/Player.java
@@ -2,6 +2,7 @@ package com.github.petrovyegor.tennisscoreboard.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
@@ -24,8 +25,9 @@ public class Player {
     private int id;
 
     @Column(name = "\"Name\"", unique = true, nullable = false)
-    @Size(min = 3, max = 30, message = "Name should be from {min} to {max} symbols")
+    @Size(min = 3, max = 15, message = "Name should be from {min} to {max} symbols")
     @NotBlank(message = "Name can not be null or contain only white spaces")
+    @Pattern(regexp = "[a-zA-Z]+", message = "The name should contain Latin letters")
     private String name;
 
     public Player(String name) {

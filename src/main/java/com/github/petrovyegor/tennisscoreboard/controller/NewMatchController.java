c4fb4036b245e5d4290ba7b0ade4cb6ee36b7ce7
PetrovYegor <YegorPetrov@outlook.com>
2025-08-12 11:03:07 +0300

    Сделал заготовку для вью match-score

--- src/main/java/com/github/petrovyegor/tennisscoreboard/controller/NewMatchController.java
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/controller/NewMatchController.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import static com.github.petrovyegor.tennisscoreboard.util.RequestAndParametersValidator.validateNewMatchPostRequest;
 
---
+++ src/main/webapp/match-score.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Match-score</title>
+</head>
+<body>
+<p>Helloooooooooooooooooooooooooooo</p>
+</body>
+</html>

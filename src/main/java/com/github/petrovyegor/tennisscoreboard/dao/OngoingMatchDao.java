0a2c24f13bc50a648502c98b682519445dbead83
PetrovYegor <YegorPetrov@outlook.com>
2025-08-17 22:47:15 +0300

    Описал метод сохранения, получения по ид и получение всех длящихся матчей в OngoingMatchDao

--- src/main/java/com/github/petrovyegor/tennisscoreboard/dao/OngoingMatchDao.java
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/dao/OngoingMatchDao.java
@@ -1,64 +1,31 @@
 package com.github.petrovyegor.tennisscoreboard.dao;
 
-import com.github.petrovyegor.tennisscoreboard.JpaUtil;
-import com.github.petrovyegor.tennisscoreboard.exception.DBException;
 import com.github.petrovyegor.tennisscoreboard.model.OngoingMatch;
-import com.github.petrovyegor.tennisscoreboard.model.Player;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class OngoingMatchDao {
-    private Map<UUID, OngoingMatch> ongoingMatches;
+    private ConcurrentHashMap<UUID, OngoingMatch> ongoingMatches;
 
     public OngoingMatchDao() {
-        ongoingMatches = new HashMap<>();
+        ongoingMatches = new ConcurrentHashMap<>();
     }
 
-    public Optional<OngoingMatch>
+    public Optional<OngoingMatch> findById(UUID id) {// погуглить методы computeIfAbsent
+        OngoingMatch ongoingMatch = ongoingMatches.get(id);
+        return Optional.ofNullable(ongoingMatch);
+    }
 
+    public OngoingMatch save(UUID id, OngoingMatch ongoingMatch) {
+        ongoingMatches.put(id, ongoingMatch);
+        return ongoingMatch;
+    }
 
-    //записать матч в мапу
-    //получить матч по ид
-}
-
-
-@Override
-public Optional<Player> findById(Integer id) {
-    try (EntityManager em = JpaUtil.getEntityManager()) {
-        Player result = em.find(Player.class, id);
-        return Optional.ofNullable(result);
-    } catch (Throwable e) {
-        throw new DBException("Failed to get player with id '%s'".formatted(id));
+    public List<OngoingMatch> findAll() {
+        return ongoingMatches.values().stream().toList();
     }
 }
 
-@Override
-public List<Player> findAll() {
-    String findAllQuery = "SELECT p FROM Player p";
-    try (EntityManager em = JpaUtil.getEntityManager()) {
-        return em.createQuery(findAllQuery, Player.class).getResultList();
-    } catch (Throwable e) {
-        throw new DBException("Failed to get all players");
-    }
-}
-
-
-@Override
-public Player save(Player entity) {
-    EntityManager em = JpaUtil.getEntityManager();
-    EntityTransaction transaction = em.getTransaction();
-    try {
-        transaction.begin();
-        em.persist(entity);
-        transaction.commit();
-        return entity;
-    } catch (Exception e) {
-        if (transaction.isActive()) {
-            transaction.rollback();
-        }
-        throw new DBException("Failed to save Player with name '%s'".formatted(entity.getName()));
-    } finally {
-        em.close();
-    }

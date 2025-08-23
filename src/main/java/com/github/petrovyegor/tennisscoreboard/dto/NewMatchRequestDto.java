4b62119ea641960c35afb11eb13a7e1f253ffc5c
PetrovYegor <YegorPetrov@outlook.com>
2025-08-23 21:45:28 +0300

    Аннотация @data в дто, можно убрать конструктор и геттеры

--- src/main/java/com/github/petrovyegor/tennisscoreboard/dto/NewMatchRequestDto.java
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/dto/NewMatchRequestDto.java
@@ -1,9 +1,7 @@
 package com.github.petrovyegor.tennisscoreboard.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,17 +12,4 @@ public class NewMatchRequestDto {
     @NotNull(message = "Name can not be null")
     @Pattern(regexp = "[a-zA-Z]{3,15}", message = "The name should contain Latin letters, name length should be from 3 to 15 symbols")
     private String secondPlayerName;
-
-    public NewMatchRequestDto(String firstPlayerName, String secondPlayerName) {
-        this.firstPlayerName = firstPlayerName;
-        this.secondPlayerName = secondPlayerName;
-    }
-
-    public String getFirstPlayerName() {
-        return firstPlayerName;
-    }
-
-    public String getSecondPlayerName() {
-        return secondPlayerName;
-    }
 }

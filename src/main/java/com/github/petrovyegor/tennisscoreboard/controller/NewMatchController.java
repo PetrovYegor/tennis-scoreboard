fb0b46a4c1ae45367cc6a492b577b513ffc4a3b0
PetrovYegor <YegorPetrov@outlook.com>
2025-08-23 21:40:16 +0300

    убрал лишние аннотации из дто

--- src/main/java/com/github/petrovyegor/tennisscoreboard/controller/NewMatchController.java
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/controller/NewMatchController.java
@@ -1,6 +1,7 @@
 package com.github.petrovyegor.tennisscoreboard.controller;
 
 import com.github.petrovyegor.tennisscoreboard.dto.NewMatchRequestDto;
+import com.github.petrovyegor.tennisscoreboard.exception.InvalidParamException;
 import com.github.petrovyegor.tennisscoreboard.service.OngoingMatchesService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,7 +18,7 @@ import java.util.UUID;
 
 @WebServlet(name = "NewMatchController", urlPatterns = "/new-match")
 public class NewMatchController extends HttpServlet {
-    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
     private final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
 
     @Override
@@ -37,7 +38,7 @@ public class NewMatchController extends HttpServlet {
         String firstPlayerName = request.getParameter("player1_name");
         String secondPlayerName = request.getParameter("player2_name");
         NewMatchRequestDto newMatchRequestDto = new NewMatchRequestDto(firstPlayerName, secondPlayerName);
-        Set<ConstraintViolation<NewMatchRequestDto>> violations = validator.validate(newMatchRequestDto);
+        vaidateRequestDto(newMatchRequestDto);
 
         UUID tempMatchId = ongoingMatchesService.prepareNewMatch(newMatchRequestDto).tempMatchId();
         //Match newMatch = newMatchService.getNewMatch(newMatchRequestDto);
@@ -53,10 +54,11 @@ public class NewMatchController extends HttpServlet {
         return source == null || source.isEmpty();
     }
 
-//    private void vaidatePostParameters(NewMatchRequestDto newMatchRequestDto) {
-//        Set<ConstraintViolation<NewMatchRequestDto>> violations = validator.validate(newMatchRequestDto);
-//        if (violations.size() != 0) {
-//            throw new InvalidParamException();
-//        }
-//    }
+    private void vaidateRequestDto(NewMatchRequestDto newMatchRequestDto) {
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<NewMatchRequestDto>> violations = validator.validate(newMatchRequestDto);
+        if (!violations.isEmpty()) {
+            throw new InvalidParamException(violations.toString());
+        }
+    }
 }
--- src/main/java/com/github/petrovyegor/tennisscoreboard/dto/NewMatchRequestDto.java
+++ src/main/java/com/github/petrovyegor/tennisscoreboard/dto/NewMatchRequestDto.java
@@ -1,19 +1,18 @@
 package com.github.petrovyegor.tennisscoreboard.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class NewMatchRequestDto {
-    @Size(min = 3, max = 15, message = "Name should be from {min} to {max} symbols")
-    @NotBlank(message = "Name can not be null or contain only white spaces")
-    @Pattern(regexp = "[a-zA-Z]+", message = "The name should contain Latin letters")
+    @NotNull(message = "Name can not be null")
+    @Pattern(regexp = "[a-zA-Z]{3,15}", message = "The name should contain Latin letters, name length should be from 3 to 15 symbols")
     private String firstPlayerName;
-    @Size(min = 3, max = 15, message = "Name should be from {min} to {max} symbols")
-    @NotBlank(message = "Name can not be null or contain only white spaces")
-    @Pattern(regexp = "[a-zA-Z]+", message = "The name should contain Latin letters")
+    @NotNull(message = "Name can not be null")
+    @Pattern(regexp = "[a-zA-Z]{3,15}", message = "The name should contain Latin letters, name length should be from 3 to 15 symbols")
     private String secondPlayerName;
 
     public NewMatchRequestDto(String firstPlayerName, String secondPlayerName) {
